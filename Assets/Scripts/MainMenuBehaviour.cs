7d2aa85479d8617e39f28c785c3c542ded16ee49
YuLin <cyyting18@gmail.com>
2025-09-23 17:24:35 +0800

    Add Level selection and credit
    Add new panel

--- Assets/Scripts/MainMenuBehaviour.cs
+++ Assets/Scripts/MainMenuBehaviour.cs
@@ -8,9 +8,18 @@ public class MainMenuBehaviour : MonoBehaviour
     /// <summary>
     /// Will load a new scene upon being called
     /// </summary>
-    /// <param name="levelName">The name of the level
-    /// we want to go to</param>
-    public void LoadLevel(string levelName)
+    /// <param name="levelName">The name of the level we want to go to</param>
+    public void LoadLevel1(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void LoadLevel2(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void LoadLevelEndless(string levelName)
     {
         SceneManager.LoadScene(levelName);
     }
